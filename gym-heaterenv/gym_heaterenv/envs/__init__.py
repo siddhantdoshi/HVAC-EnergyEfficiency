2064b3313d28a1014244ce7a530a3b17fda3068e
Siddhant Doshi <siddhant.doshi@gmail.com>
2020-08-11 16:54:46 +0530

    Created new simulation and environment

--- gym-heaterenv/gym_heaterenv/envs/__init__.py
+++ gym-heaterenv/gym_heaterenv/envs/__init__.py
@@ -3,5 +3,6 @@ from gym_heaterenv.envs.heater_env2 import HeaterEnv2
 from gym_heaterenv.envs.heater_env3 import HeaterEnv3
 from gym_heaterenv.envs.heater_env4 import HeaterEnv4
 from gym_heaterenv.envs.heater_env5 import HeaterEnv5
+from gym_heaterenv.envs.heater_env6 import HeaterEnv6
 
 from gym_heaterenv.envs.heater_env_relay import HeaterEnvRelay
