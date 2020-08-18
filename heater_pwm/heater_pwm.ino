0df5735626b56318a3ff76d2e1a952a098c407fe
Rutvik Doshi <rutvikweb@gmail.com>
2020-08-18 10:16:54 +0530

    synced arduino code

--- heater_pwm/heater_pwm.ino
+++ heater_pwm/heater_pwm.ino
@@ -5,7 +5,11 @@
 #define CS_PIN A1 // Driver Board CS
 #define THERMISTOR_PIN A2 // Thermistor (temperature sensor)
 
+int count = 0;
+
 float current = 0.0;
+float resistance = 3.6;
+float max_voltage = 12.0;
 
 float set_point = 30.0;
 
@@ -14,12 +18,66 @@ float prev_error = 0.0;
 float derivative = 0.0;
 float integral = 0.0;
 
-float kp = 15000.0;
-float ki = 50.0;
-float kd = -500.0;
+float kp = 18.0;
+float ki = 0.004;
+float kd = 10.0;
 
 float output_power = 0.0;
 int pwm_output = 0;
+
+const int num_sensor_readings = 100;
+int temp_val[num_sensor_readings];
+
+float temp_sort_clean()
+{
+  int temp = 0;
+  for (int i = 0; i < (num_sensor_readings - 1); i++)
+  {
+    for (int j = 0; j < (num_sensor_readings - 1 - i); j++)
+    {
+      if (temp_val[j] < temp_val[j + 1])
+      {
+        temp = temp_val[j];
+        temp_val[j] = temp_val[j + 1];
+        temp_val[j + 1] = temp;
+      }
+    }
+  }
+
+//  int sum = 0;
+//  int count = 0;
+//  
+//  for (int i = (num_sensor_readings / 3); i < (num_sensor_readings * 2 / 3); i++)
+//  {
+//    sum += temp_val[i];
+//    count ++;
+//  }
+
+//  return ((sum + 0.0) / count);
+
+    return temp_val[49];
+}
+
+float get_temp()
+{
+  float sensor_average = 0;
+
+  for (int i = 0; i < num_sensor_readings; i++)
+  {
+    int sensor_val = analogRead(THERMISTOR_PIN);
+    temp_val[i] = sensor_val;
+    delay(1000 / num_sensor_readings);
+  }
+      
+  sensor_average = temp_sort_clean();
+  
+  float voltage = sensor_average * 5.0 / 1024.0;
+  float resistance = 10000.0 * (voltage / (5.0 - voltage));
+  float temp_reciprocal = 1.0 / 295.15 + log(resistance / 10000.0) / 3950.0;
+  float temperature = (1.0 / temp_reciprocal) - 273.15;
+
+  return temperature;
+}
 
 void setup()                         
 {
@@ -35,42 +93,46 @@ void setup()
   
   analogWrite(HEATER_PWM, 0); // Sets heater to off
 
-  prev_error = set_point - analogRead(HEATER_PWM);
+//  set_point = get_temp() + 10.0;
+  prev_error = 10;
 
   Serial.begin(9600); // Initiates the serial to do the monitoring
+
+//  Serial.println(set_point);
+  Serial.println("Timestamp, Temperature (ºC), Error,  Output power (W), PWM output, Current, PWM / Current");
 }
 
-float get_temp()
-{
-  float sensor_average = 0;
 
-  for (int i = 0; i < 10; i++)
-  {
-    int sensor_val = analogRead(THERMISTOR_PIN);
-    sensor_average += sensor_val;
-    delay(5);
-  }  
-  sensor_average /= 10.0;
-  
-  float voltage = sensor_average * 5.0 / 1024.0;
-  float resistance = 10000.0 * (voltage / (5.0 - voltage));
-  float temp_reciprocal = 1.0 / 295.15 + log(resistance / 10000.0) / 3950.0;
-  float temperature = (1.0 / temp_reciprocal) - 273.15;
-
-  return temperature;
-}
 
 void loop() 
 {
   float temperature = get_temp();
+
+  if (count == 0)
+  {
+    set_point = temperature + 10;
+    Serial.println(set_point);
+    count ++;
+  }
+  
   error = set_point - temperature;
 
-  derivative = error - prev_error;
-  if (error < 3)
-  {
-    integral += error;
-  }
+  current = analogRead(CS_PIN) * 5.0 / (1024 * 0.13) - 0.189;
 
+  derivative = error - prev_error;
+  
+  if (temperature >= 0.9 * set_point) // and temperature <= set_point)
+  {
+//    if (derivative <= 0)
+//    {
+//      integral += error;
+//    } else
+//    {
+//      integral += 0.8 * error;
+//    }
+      integral += error;
+  }
+  
   output_power = kp * error + ki * integral + kd * derivative;
 
   if (output_power < 0)
@@ -79,31 +141,38 @@ void loop()
   }
   else
   {
-    pwm_output = (int) (sqrt(output_power) + 0.5);
+    pwm_output = (int) (sqrt(resistance * output_power) + 0.5) * 255.0 / max_voltage;
   }
 
   if (pwm_output > 255)
   {
     pwm_output = 255;
   }
-
-  current = analogRead(CS_PIN) * 5.0 / (1024 * 0.13);
-  analogWrite(HEATER_PWM, pwm_output);
-
-  prev_error = error;
+// Added code
+  if (temperature < 30.0){
+    analogWrite(HEATER_PWM, 255);  
+  }else{
+   analogWrite(HEATER_PWM, 0);  
+  }
   
-  Serial.print("Temperature: ");
+// End of added code
+  prev_error = error;
   Serial.print(temperature);
-  Serial.print(";  Error: ");
+  
+  Serial.print(", ");
   Serial.print(error);
-  Serial.print(";  Output power: ");
+  
+  Serial.print(", ");
   Serial.print(output_power);
-  Serial.print(";  PWM output: ");
+  
+  Serial.print(", ");
   Serial.print(pwm_output);
-  Serial.print("; Current: ");
+  
+  Serial.print(", ");
   Serial.print(current);
-  Serial.print("; PWM / Current: ");
+  
+  Serial.print(", ");
   Serial.println(pwm_output / current);
 
-  delay(200);
+//  delay(200);
 }
--- relay_temperature_control/relay_temperature_control.ino
+++ relay_temperature_control/relay_temperature_control.ino
@@ -2,25 +2,74 @@ const int thermistor_pin = A0;
 const int relay_pin = 8;
 
 float set_point = 30.0;
+long tot_energy = 0.0;
+bool reach_target = false;
+double squared_error = 0.0;
+double mean_squared_error = 0.0;
+double efficiency_metric = 0.0;
+int num_samples = 0;
+
+const int num_sensor_readings = 100;
+int temp_val[num_sensor_readings];
 
 void setup()
 {
+  reach_target = false;
   pinMode(thermistor_pin, INPUT);
   pinMode(relay_pin, OUTPUT);
   Serial.begin(9600);
   digitalWrite(relay_pin, HIGH);
 }
 
+// Ignoring extreme sensor values
+float temp_sort_clean()
+{
+  int temp = 0;
+  for (int i = 0; i < (num_sensor_readings - 1); i++)
+  {
+    for (int j = 0; j < (num_sensor_readings - 1 - i); j++)
+    {
+      if (temp_val[j] < temp_val[j + 1])
+      {
+        temp = temp_val[j];
+        temp_val[j] = temp_val[j + 1];
+        temp_val[j + 1] = temp;
+      }
+    }
+  }
+
+  int sum = 0;
+  int count = 0;
+  
+  for (int i = (num_sensor_readings / 3); i < (num_sensor_readings * 2 / 3); i++)
+  {
+//    Serial.print(i);
+//    Serial.print(", ");
+//    Serial.print(temp_val[i]);
+//    Serial.print(", ");
+//    Serial.println(sum);
+
+    sum += temp_val[i];
+    count ++;
+  }
+
+  return ((sum + 0.0) / count);
+}
+
 float get_temp()
 {
   float sensor_average = 0;
 
-  for (int i = 0; i < 2; i++)
+  for (int i = 0; i < num_sensor_readings; i++)
   {
     int sensor_val = analogRead(thermistor_pin);
-    sensor_average += sensor_val;
-  }  
-  sensor_average /= 2.0;
+    temp_val[i] = sensor_val;
+//    Serial.print(i);
+//    Serial.print(", ");
+//    Serial.println(temp_val[i]);
+    delay(1000 / num_sensor_readings);
+  }    
+  sensor_average = temp_sort_clean();
   
   float voltage = sensor_average * 5.0 / 1024.0;
   float resistance = 10000.0 * (voltage / (5.0 - voltage));
@@ -30,20 +79,52 @@ float get_temp()
   return temperature;
 }
 
+float old_temp = 0;
 void loop()
 {
+  num_samples ++;
   float temperature = get_temp();
+  
   Serial.print("Temperature: ");
-  Serial.println(temperature);
+  Serial.print(temperature);
+  Serial.print(";  Num Samples: ");
+  Serial.print(num_samples);
+  
+  
+  old_temp = temperature;
 
   if (temperature < set_point - 0.1)
   {
     digitalWrite(relay_pin, LOW);
+    tot_energy += 255L * 255L;
   }
   if (temperature > set_point + 0.1)
   {
     digitalWrite(relay_pin, HIGH);  
   }
+  if (temperature >= set_point)
+  {
+    reach_target = true;
+  }
+  
+  if (reach_target)
+  {
+    squared_error += (temperature - set_point) * (temperature - set_point);
+  }
 
-  delay(300);
+  mean_squared_error = squared_error / num_samples;
+  efficiency_metric = (0.7 * tot_energy) + (0.3 * mean_squared_error);
+  
+  Serial.print(";  Total energy: ");
+  Serial.print(tot_energy);
+
+  Serial.print(";  Squared error: ");
+  Serial.print(squared_error);
+
+  Serial.print(";  Mean squared error: ");
+  Serial.print(mean_squared_error);
+  
+  Serial.print(";  Efficiency metric: ");
+  Serial.println(efficiency_metric);
+//  delay(1000);
 }
